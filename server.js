eca677d7427f083d5c832ebb2ee2d7457678bb2d
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-07-18 11:00:27 +0530

    BroadCast chat
    Simple Chat room that is open and can be joined by entering name.

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+	console.log(`Listening at port ${PORT}`);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+//Socket
+const io = require("socket.io")(http);
+io.on("connection", (socket) => {
+	console.log("Connected...");
+	socket.on("send message", (msg) => {
+		socket.broadcast.emit("send message", msg);
+	});
+	socket.on("user connection", (msg) => {
+		socket.li_chat_name = msg.user;
+		socket.broadcast.emit("user connection", msg);
+	});
+
+	socket.on("disconnect", () => {
+		let msg = {
+			user: socket.li_chat_name,
+			type: "left",
+		};
+		socket.broadcast.emit("user connection", msg);
+	});
+});
